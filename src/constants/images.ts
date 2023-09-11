4f1357edc64e2348cd34a4e3c534e7909904e06f
TULENp <zh_kononenko3@mail.ru>
2023-09-11 14:20:39 +0300

    Change MainPage, Add text, search, userContainer Add constants, icons

---
+++ src/constants/images.ts
@@ -0,0 +1,2 @@
+export const IconCross = '/src/assets/cross.svg';
+export const IconPlus = '/src/assets/plus.svg';
--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,19 +1,68 @@
-import React from 'react';
+import { useState } from 'react';
 import mainStyles from './MainPage.module.css';
 import { UserCard } from '../../components/UserCard';
 
+//* Display main page with search bar and team members
 export function MainPage() {
+    const [searchValue, setSearchValue] = useState('');
+
     return (
-        <div>
-            <h1>mainpage</h1>
-            <UserCard
-                user={{
-                    id: 0,
-                    name: 'name',
-                    email: 'qwerty@email.com',
-                    address: { city: 'moscow' },
-                }}
-            />
+        <div className={mainStyles.container}>
+            <h1>Команда организации</h1>
+            <h4>
+                Владельцы команд могут добавлять участников в команду своей
+                организации, добавляя их адреса электронной почты. У них должна
+                быть учетная запись на сайте.
+            </h4>
+            {/* Search bar */}
+            <div>
+                <h4 className={mainStyles.searchLabel}>
+                    Введите e-mail участника
+                </h4>
+                <section className={mainStyles.searchBarContainer}>
+                    <input
+                        className={mainStyles.search}
+                        type='search'
+                        placeholder='example@gmail.com'
+                        value={searchValue}
+                        onChange={(e) => setSearchValue(e.target.value)}
+                    />
+                </section>
+            </div>
+            <section className={mainStyles.usersContainer}>
+                <UserCard
+                    user={{
+                        id: 0,
+                        name: 'name',
+                        email: 'qwerty@email.com',
+                        address: { city: 'moscow' },
+                    }}
+                />
+                <UserCard
+                    user={{
+                        id: 0,
+                        name: 'name',
+                        email: 'qwerty@email.com',
+                        address: { city: 'moscow' },
+                    }}
+                />
+                <UserCard
+                    user={{
+                        id: 0,
+                        name: 'name',
+                        email: 'qwerty@email.com',
+                        address: { city: 'moscow' },
+                    }}
+                />
+                <UserCard
+                    user={{
+                        id: 0,
+                        name: 'name',
+                        email: 'qwerty@email.com',
+                        address: { city: 'moscow' },
+                    }}
+                />
+            </section>
         </div>
     );
 }
